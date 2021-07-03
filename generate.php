2ea98929b942712a1c2ea0e3f8104369cd1135bb
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-07-04 03:15:26 +0900

    Initial commit

---
+++ generate.php
@@ -0,0 +1,23 @@
+<?php
+
+function generateAttribute(bool $unicode, string $version = 'latest'): string
+{
+    $content = file_get_contents(sprintf(
+        "https://unpkg.com/filter-validate-email@%s/lib/regexp/%s.js",
+        $version,
+        $unicode ? 'unicode' : 'ascii'
+    ));
+
+    if (!preg_match('#^var _default = /(.*)/[a-z]+;$#im', $content, $match)) {
+        throw new \RuntimeException('Invalid Response.');
+    }
+
+    return htmlspecialchars(strtr($match[1], ['a-z' => 'a-zA-Z', 'a-f' => 'a-fA-F']), ENT_QUOTES);
+}
+
+echo "# HTML Pattern Attribute (Unicode)\n";
+echo generateAttribute(true, $_SERVER['argv'][1] ?? 'latest') . "\n";
+echo "\n";
+echo "# HTML Pattern Attribute (ascii)\n";
+echo generateAttribute(false, $_SERVER['argv'][1] ?? 'latest') . "\n";
+echo "\n";
